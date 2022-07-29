e74cbdb443777953e6919df75c09a58a5c2bb3fd
Damion Russell <domino.russell@gmail.com>
2022-07-29 11:59:45 -0400

    exercise one complete

--- ORM Dapper exercise/Program.cs
+++ ORM Dapper exercise/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+using ORM_Dapper_exercise;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var config = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json")
+                        .Build();
+
+        string connString = config.GetConnectionString("DefaultConnection");
+
+        IDbConnection conn = new MySqlConnection(connString);
+        var repo = new DapperDepartmentRepository(conn);
+
+        Console.WriteLine("Type a new Department Name");
+
+        var newDepartment = Console.ReadLine();
+
+        repo.InsertDepartment(newDepartment);
+
+        var departments = repo.GetAllDepartments();
+        foreach (var item in departments)
+        {
+            Console.WriteLine(item.Name);
+        }
+    }
+}
